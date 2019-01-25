bd2767451a515f46733bf1d250f8b8240d125e58
karolux4 <karolisbutkus4@gmail.com>
2019-01-25 23:41:38 +0200

    Created Unity project

---
+++ House builder/Assets/Scripts/Drag_and_Drop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag_and_Drop : MonoBehaviour
+{
+    public GameObject Room_block;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MousePosition()
+    {
+        float MouseX = Input.mousePosition.x;
+        float MouseY = Input.mousePosition.y;
+        // snaping block to grid
+        Round_values(ref MouseX, ref MouseY);
+    }
+    private void Round_values(ref float x, ref float y)
+    {
+        if (x >= 0)
+        {
+            x = (int)x + 0.5f;
+        }
+        else
+        {
+            x = Mathf.Sign(x) * (Mathf.Abs((int)x) + 0.5f);
+        }
+        if (y >= 0)
+        {
+            y = (int)y + 0.5f;
+        }
+        else
+        {
+            y = Mathf.Sign(y) * ((int)Mathf.Abs(y) + 0.5f);
+        }
+    }
+}
