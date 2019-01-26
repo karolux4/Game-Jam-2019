6eb5d618755c66e921c44aa68f6f8c371da3a75a
karolux4 <karolisbutkus4@gmail.com>
2019-01-26 21:37:57 +0200

    Created room instatiation script

--- House builder/Assets/Scripts/Room.cs
+++ House builder/Assets/Scripts/Room.cs
@@ -8,12 +8,16 @@ public class Room
     public int price{get;set;}
     public float quality{get;set;}
     public float safety { get; set; }
+    public float length { get; set; }
+    public float height { get; set; }
 
-    public Room( string name, RoomType type, int price, float quality, float safety){
+    public Room( string name, RoomType type, int price, float quality, float safety, float length, float height){
         this.name = name;
         this.type = type;
         this.price = price;
         this.quality = quality;
+        this.length = length;
+        this.height = height;
     }
 }
 
---
+++ House builder/Assets/Scripts/Room_Instantiation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_Instantiation : MonoBehaviour
+{
+    public GameObject Room_prefab;
+    public GameObject Level_information;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject room = Instantiate(Room_prefab);
+        Rigidbody2D rd = room.AddComponent<Rigidbody2D>();
+        rd.bodyType = RigidbodyType2D.Kinematic;
+        BoxCollider2D col = room.AddComponent<BoxCollider2D>();
+        col.isTrigger = true;
+        //needs to ajust BoxCollider according to length and height
+        //col.offset = new Vector2(0f,height/2);
+        // col.size = new Vector2(length, height);
+        Drag_and_Drop DaD = room.AddComponent<Drag_and_Drop>();
+        DaD.Room_block = this.gameObject;
+       // DaD.Room_height = height;
+       // DaD.Room_length = length;
+        DaD.level_information = Level_information;
+    }
+}
